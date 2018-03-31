2bebdeba1e03d45d1525e51b54345d85b8cf3f82
mamenjivar <menjivarmiguel221@gmail.com>
2018-03-31 12:35:54 -0700

    first commit for hw #1 assignment

---
+++ hwOne.c
@@ -0,0 +1,17 @@
+/*
+ * hwOne.c
+ *
+ * Assignment: HW #1
+ *
+ *  Created on: Mar 31, 2018
+ *      Author: Miguel Menjivar
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+	printf("Hello World");
+	return 0;
+}
+
