27502b4f030e5b36304b10e486401db789a15781
mamenjivar <menjivarmiguel221@gmail.com>
2018-06-14 18:56:50 -0700

    deleting a folder because it has been moved to another location, more specifically under project 3

--- Final/dynamic/html/search/all_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['operator_25',['operator%',['../class_big_numbers_1_1_functions.html#afd45fb8f345a0ad766f548909938abd0',1,'BigNumbers::Functions']]],
-  ['operator_2a',['operator*',['../class_big_numbers_1_1_functions.html#ac362857dcd2b6f7ab3aef9452fc113ec',1,'BigNumbers::Functions']]],
-  ['operator_2b',['operator+',['../class_big_numbers_1_1_functions.html#a12347e08db929456aaa51361808b10f7',1,'BigNumbers::Functions']]],
-  ['operator_2d',['operator-',['../class_big_numbers_1_1_functions.html#a5f9ce304346fe3056050cbf30b8ab7d0',1,'BigNumbers::Functions']]],
-  ['operator_2f',['operator/',['../class_big_numbers_1_1_functions.html#a4b5e876a4a9ca1a14df0a1c646ea3019',1,'BigNumbers::Functions']]]
-];
--- Final/dynamic/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print',['print',['../class_big_numbers_1_1_functions.html#a2badbd444a636298fadbdf3525cfba30',1,'BigNumbers::Functions']]]
-];
