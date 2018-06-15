d9146212e5e851140c6b2d29a177fa7a36050022
mamenjivar <menjivarmiguel221@gmail.com>
2018-06-14 18:56:00 -0700

    uploading and deleting a few files

--- Final/static/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mybignumbers',['MyBigNumbers',['../class_big_numbers_1_1_my_big_numbers.html',1,'BigNumbers::MyBigNumbers'],['../class_big_numbers_1_1_my_big_numbers.html#ad696cd852930539fa16f4c21db78e1f4',1,'BigNumbers::MyBigNumbers::MyBigNumbers()'],['../class_big_numbers_1_1_my_big_numbers.html#a48cb28d06eb08426318b7a44bf77c549',1,'BigNumbers::MyBigNumbers::MyBigNumbers(int)']]]
-];
--- Final/static/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mybignumbers',['MyBigNumbers',['../class_big_numbers_1_1_my_big_numbers.html',1,'BigNumbers']]]
-];
